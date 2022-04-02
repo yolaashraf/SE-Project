85f39c1b7c19a70171220f793d52cd0ae016b02f
yolaashraf <yola1906219@miuegypt.edu.eg>
2022-04-02 19:37:47 +0200

    root changes

--- app/config/config.php
+++ app/config/config.php
@@ -9,7 +9,7 @@ define('DB_NAME', 'el-setat-market');
 //echo dirname(dirname(__FILE__));
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://localhost/mvc/public/');
+define('URLROOT', 'http://localhost/SE-Project/public/');
 // Site Name
 define('SITENAME', 'EL SETAT MARKET');
 
