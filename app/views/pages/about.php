0579f1297ad1e601faf6c84125d7deeb153913d0
youssefawad001 <youssef001@gmail.com>
2022-04-03 10:09:51 +0200

    about us & contact us

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,22 +1,69 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>About Us</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+	<link rel="stylesheet" type="text/css" href="../../public/css/aboutstyle.css">
+</head>	
+</body>
+</html>
+
 <?php
-class About extends view
+class About extends View
 {
-
-  public function output()
-  {
-    $title = $this->model->title;
-    $data = $this->model->data;
-
-    require APPROOT . '/views/inc/header.php';
+	Public function output()
+	{		
+		require APPROOT . '/views/inc/header.php';
     $text = <<<EOT
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-      <h1 class="display-4"> $title</h1>
-      <h2 class="display-4"> $data</h>
-    </div>
-  </div>
+<div class="section">
+	<div class="container">
+	<div class="title">
+	<h1>About Us</h1>
+	</div>
+	<div class="content-section">
+				
+        </div>
+		<div class="content">
+		  <h4>Social initiative by Mofida Shiha to empower young 
+          Egyptian female designers and innovators through providing the 
+          appropriate environment that introduce them to the market.</h4>
+		  <p>We all know about the famous TV Program (الستات مايعرفوش يكدبو)
+          This was the start for the program host Mofida
+          Shiha to recognize the need for supporting the
+          female talents that she host a segment of them
+          during her program which is working for the sake
+          of them and covering all there interests.
+          Mofida takes her recognition a step forward to be
+          a Social initiative to empower young Egyptian
+          female designers and innovators through
+          providing the appropriate environment that
+          introduce them to the market by promoting the
+          community knowledge of fashion and style.</p>
+		</div>
+
+      
+        <div class="images-section">
+			<img src="../../images/img1.jpg">
+		</div>
+
+		<div class="social">
+			<h2><a href="https://web.facebook.com/elsetatmarket/?ref=page_internal"><i class="fab fa-facebook-f"></i></a> <a href="https://www.instagram.com/elsetatmarket/"><i class="fab fa-instagram"></i></a></h2>
+		</div>
+	</div>
+	</div>
+</div>
+
+
 EOT;
     echo $text;
     require APPROOT . '/views/inc/footer.php';
-  }
+		
+	}
+	
 }
--- app/views/pages/contact.php
+++ app/views/pages/contact.php
@@ -1,28 +1,69 @@
+<!DOCTYPE html>
+<!-- Created By CodingLab - www.codinglabweb.com -->
+<html>
+  <head>
+
+     <title>Contact Us</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+	<link rel="stylesheet" type="text/css" href="../../public/css/contactstyle.css">
+   </head>
+</html>
 <?php
 class Contact extends View
 {
 	Public function output()
 	{
-		$title = $this->model->title;
-		
-		require APPROOT . '/views/inc/header.php';
+    require APPROOT . '/views/inc/header.php';
     $text = <<<EOT
-    <div class="jumbotron jumbotron-fluid">
+    
+    <section>
     <div class="container">
-      <h1 class="display-4"> $title</h1>
+    
+    <div class="content">
+      <div class="left-side">
+      <div class="About Us">
+      <div class="topic"><h2>Contact Us</h2></div>
+
+    </div>
+        <div class="phone details">
+          <i class="fas fa-phone-alt"></i>
+          <div class="topic">Phone</div>
+          <div class="text-one">01220030303</div>
+          
+        </div>
+        <div class="email details">
+          <i class="fas fa-envelope"></i>
+          <div class="topic">Email</div>
+          <div class="text-one">elsetatmarket@gmail.com</div>
+          
+        </div>
+      </div>
+      <div class="right-side">
+        <div class="topic-text">Send us a message</div>
+        <p>Feel free to get in touch with us, you can send me message from here. It's my pleasure to help you.</p>
+      <form action="#">
+        <div class="input-box">
+          <input type="text" placeholder="Enter your name">
+        </div>
+        <div class="input-box">
+          <input type="text" placeholder="Enter your email">
+        </div>
+        <div class="input-box message-box">
+          
+        </div>
+        <div class="button">
+          <input type="button" value="Send Now" >
+        </div>
+      </form>
+    </div>
     </div>
   </div>
-EOT;
-    echo $text;
-    require APPROOT . '/views/inc/footer.php';
-		
-	}
-	
+  </section>
+  EOT;
+  echo $text;
+  require APPROOT . '/views/inc/footer.php';
+  
 }
 
-
-
-
-
-
-?>
+}
