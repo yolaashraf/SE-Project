188ee8db20beb74f759759bd6bf8092c8017689b
youssefawad001 <youssef001@gmail.com>
2022-05-11 21:36:08 +0200

    about us

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -3,6 +3,25 @@
 
 <head>
 
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>About us</title>
+<meta charset="utf-8">
+
+    <link rel="stylesheet" href="../../public/css/default.css" media="screen">
+<link rel="stylesheet" href="../../public/css/About-us.css" media="screen">
+    <script class="u-script" type="text/javascript" src="../../../public/js/jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="../../../public/js/default.js" defer=""></script>
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+</head>
+</html>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>About Us</title>
@@ -21,43 +40,40 @@ class About extends View
 	{		
 		require APPROOT . '/views/inc/header.php';
     $text = <<<EOT
-<div class="section">
-	<div class="container">
-	<div class="title">
-	<h1><center>About Us</center></h1>
-	</div>
-	<div class="content-section">
-				
+	<body class="u-body u-xl-mode">
+    <section class="u-clearfix u-section-1" id="carousel_f896">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-clearfix u-expanded-width u-gutter-0 u-layout-wrap u-layout-wrap-1">
+          <div class="u-layout" style="">
+            <div class="u-layout-row" style="">
+              <div class="u-align-center u-container-style u-grey-5 u-layout-cell u-size-23 u-layout-cell-1">
+                <div class="u-container-layout u-valign-middle u-container-layout-1">
+                  <h4 class="u-text u-text-1">about</h4>
+                </div>
+              </div>
+              <div class="u-align-center u-container-style u-grey-5 u-layout-cell u-size-37 u-layout-cell-2">
+                <div class="u-container-layout u-container-layout-2">
+                  <p class="u-align-center u-text u-text-2">Social initiative by Mofida Shiha to empower young Egyptian female designers and innovators through providing the appropriate environment that introduce them to the market.<br>
+                    <br> We all know about the famous TV Program (الستات مايعرفوش يكدبو) This was the start for the program host Mofida Shiha to recognize the need for supporting the female talents that she host a segment of them during her program which is working for the sake of them and covering all there interests.<br>
+                    <br>&nbsp;Mofida takes her recognition a step forward to be a Social initiative to empower young Egyptian female designers and innovators through providing the appropriate environment that introduce them to the market by promoting the community knowledge of fashion and style.
+                  </p>
+                  <div class="u-social-icons u-spacing-25 u-social-icons-1">
+                    <a class="u-social-url" target="_blank" href="https://www.facebook.com/elsetatmarket/?ref=page_internal" title="facebook"><span class="u-icon u-icon-circle u-social-facebook u-social-icon u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-b57b"></use></svg><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content" viewBox="0 0 112 112" x="0px" y="0px" id="svg-b57b"><path d="M75.5,28.8H65.4c-1.5,0-4,0.9-4,4.3v9.4h13.9l-1.5,15.8H61.4v45.1H42.8V58.3h-8.8V42.4h8.8V32.2 c0-7.4,3.4-18.8,18.8-18.8h13.8v15.4H75.5z"></path></svg></span>
+                    </a>
+                    <a class="u-social-url" target="_blank" href="https://www.instagram.com/elsetatmarket/" title="instagram"><span class="u-icon u-icon-circle u-social-icon u-social-instagram u-icon-2"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-5333"></use></svg><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xml:space="preserve" class="u-svg-content" viewBox="0 0 112 112" x="0px" y="0px" id="svg-5333"><path d="M55.9,32.9c-12.8,0-23.2,10.4-23.2,23.2s10.4,23.2,23.2,23.2s23.2-10.4,23.2-23.2S68.7,32.9,55.9,32.9z M55.9,69.4c-7.4,0-13.3-6-13.3-13.3c-0.1-7.4,6-13.3,13.3-13.3s13.3,6,13.3,13.3C69.3,63.5,63.3,69.4,55.9,69.4z"></path><path d="M79.7,26.8c-3,0-5.4,2.5-5.4,5.4s2.5,5.4,5.4,5.4c3,0,5.4-2.5,5.4-5.4S82.7,26.8,79.7,26.8z"></path><path d="M78.2,11H33.5C21,11,10.8,21.3,10.8,33.7v44.7c0,12.6,10.2,22.8,22.7,22.8h44.7c12.6,0,22.7-10.2,22.7-22.7 V33.7C100.8,21.1,90.6,11,78.2,11z M91,78.4c0,7.1-5.8,12.8-12.8,12.8H33.5c-7.1,0-12.8-5.8-12.8-12.8V33.7 c0-7.1,5.8-12.8,12.8-12.8h44.7c7.1,0,12.8,5.8,12.8,12.8V78.4z"></path></svg></span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-		<div class="content">
-		  <h4><center>Social initiative by Mofida Shiha to empower young 
-          Egyptian female designers and innovators through providing the 
-          appropriate environment that introduce them to the market.</center></h4>
-		  <p><center>We all know about the famous TV Program (الستات مايعرفوش يكدبو)
-          This was the start for the program host Mofida
-          Shiha to recognize the need for supporting the
-          female talents that she host a segment of them
-          during her program which is working for the sake
-          of them and covering all there interests.
-          Mofida takes her recognition a step forward to be
-          a Social initiative to empower young Egyptian
-          female designers and innovators through
-          providing the appropriate environment that
-          introduce them to the market by promoting the
-          community knowledge of fashion and style.</center></p>
-		</div>
+      </div>
+    </section>
+    
+    
 
-      
-        <div class="images-section">
-			<img src="../../images/img1.jpg">
-		</div>
-
-		<div class="social">
-			<h2><a href="https://web.facebook.com/elsetatmarket/?ref=page_internal"><i class="fab fa-facebook-f"></i></a> <a href="https://www.instagram.com/elsetatmarket/"><i class="fab fa-instagram"></i></a></h2>
-		</div>
-	</div>
-	</div>
-</div>
+  </body>
 
 
 EOT;
@@ -67,3 +83,4 @@ EOT;
 	}
 	
 }
+
